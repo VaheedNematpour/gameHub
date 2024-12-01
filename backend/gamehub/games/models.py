e2f1578a89f25cb58e0485f0d605e4c3ecba96d3
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-01 08:58:15 +0330

    Implement models for our backend

---
+++ backend/gamehub/games/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Platform(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    score = models.PositiveSmallIntegerField()
+    platfroms = models.ForeignKey(Platform, on_delete=models.PROTECT)
+
