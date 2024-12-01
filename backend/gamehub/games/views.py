50a24b79c24ef9c0c051f130e7fb6d249715eb6c
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-01 13:00:39 +0330

    Implement posting and mutating categories and platforms

--- backend/gamehub/games/views.py
+++ backend/gamehub/games/views.py
@@ -1,40 +1,69 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category, Platform, Game
 from .serializers import CategorySerializer, PlatformSerializer, GameSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def category_list(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'DELETE'])
 def category_detail(request, id):
     category = get_object_or_404(Category, pk=id)
-    serializer = CategorySerializer(category)
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if category.game_set.count() > 0:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        category.delete()
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def platform_list(request):
-    platforms = Platform.objects.all()
-    serializer = PlatformSerializer(platforms, many=True)
+    if request.method == 'GET':
+        platforms = Platform.objects.all()
+        serializer = PlatformSerializer(platforms, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = PlatformSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'DELETe'])
 def platform_detail(request, id):
     platform = get_object_or_404(Platform, pk=id)
-    serializer = PlatformSerializer(platform)
+    if request.method == 'GET':
+        serializer = PlatformSerializer(platform)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if platform.game_set.count() > 0:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        platform.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
