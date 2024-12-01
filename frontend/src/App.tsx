736ddb5d08048bdb920acfdda7dfea9033116e48
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-01 18:04:58 +0330

    Fix the problem with tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [dark, setDark] = useState(false);
+
   return (
-    <>
-      <header></header>
+    <div className={`h-screen ${dark ? "bg-gray-800" : "bg-gray-100"}`}>
+      <header className="px-8 py-12"></header>
       <main></main>
-    </>
+    </div>
   );
 }
 
