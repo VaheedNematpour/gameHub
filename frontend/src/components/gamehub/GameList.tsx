3b01028e3c34c8241314ea83a453e5039087623f
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-12-07 10:21:54 +0330

    Fix some bugs

--- frontend/src/components/gamehub/GameList.tsx
+++ frontend/src/components/gamehub/GameList.tsx
@@ -8,20 +8,17 @@ interface Props {
 
 function GameList({ isDark }: Props) {
   const { data, error, isLoading } = useGame();
+  const numbers = [1, 2, 3, 4, 5, 6];
 
   if (error) return <p>{error.message}</p>;
   if (isLoading)
     return (
-      <ul className="grid md:grid-cols-2 space-x-3 lg:grid-cols-3">
-        <li>
-          <LoadingSkeleton />
-        </li>
-        <li>
-          <LoadingSkeleton />
-        </li>
-        <li>
-          <LoadingSkeleton />
-        </li>
+      <ul className="grid md:grid-cols-2 space-x-3 my-3 lg:grid-cols-3">
+        {numbers.map((number) => (
+          <li key={number} className="my-3 mx-3">
+            <LoadingSkeleton />
+          </li>
+        ))}
       </ul>
     );
 
@@ -41,7 +38,7 @@ function GameList({ isDark }: Props) {
             <li
               className={`px-3 text-lg ${
                 isDark ? "text-gray-300" : "text-gray-800"
-              } xl:text-xl`}
+              } my-3 xl:text-xl`}
               key={game.id}
             >
               <GameCard game={game} />
